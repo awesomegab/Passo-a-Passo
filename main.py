a4c557b07c7f68977e311f43ba77a6bb1e4435d7
awesomegab <gm100327@gmail.com>
2025-03-13 10:05:50 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# Ciência de Dados Passo a Passo
+
+import streamlit as st
+
+st.logo("MÉTODO.png")
+
+st.title("Domine a Ciência de Dados com um Guia Prático e Direto ao Ponto!")
+st.text("""""")
+
+st.subheader("Aprenda passo a passo a aplicar Ciência de Dados com Python, mesmo que esteja começando do zero!")
+st.text("""""")
+
+c1, c2, c3 = st.columns(3)
+
+with c2:
+    st.image("image.png")
+
+    st.link_button("Quero aprender!", "https://pay.kiwify.com.br/KffaxVH", type='primary')
+
+st.text("""""")
+
+st.text("""Com este livro você aprenderá a estruturar projetos de Ciência de Dados, trabalhar com dados reais e aplicar Machine Learning, tudo com exemplos práticos e código em Python!""")
+
+st.text("""Domine as etapas essenciais de um projeto de Ciência de Dados""")
+st.text("""Aprenda a programar com Python de forma aplicada""")
+st.text("""Faça projetos práticos para turbinar seu portfólio""")
+st.text("""Use o conhecimento para competir em desafios ou se destacar no mercado""")
